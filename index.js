8bed0c0cc9a312d9a473c6a539ed404ca5eed7ad
Kazi Rasel <kazirasel50@gmail.com>
2022-03-07 20:44:14 +0600

    make it better

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-const express = require("express");
-const app = express();
-const port = 4000;
+// http module
+const http = require('http');
+const fs = require('fs');
+const port = process.env.port;
+const hostname = '127.0.0.1';
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+
+const myServer = http.createServer((req,res)=> {
+
+    if (req.url === "/") {
+        fs.readFile("index.html", (err, data) => {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.write(data);
+            res.end();
+        });
+    } else if (req.url === "/about") {
+        fs.readFile("about.html", (err, data) => {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.write(data);
+            res.end();
+        });
+    } else if (req.url === "/contact") {
+        fs.readFile("contact.html", (err, data) => {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.write(data);
+            res.end();
+        });
+    } else {
+        fs.readFile("error.html", (err, data) => {
+            res.writeHead(404, { "Content-Type": "text/html" });
+            res.write(data);
+            res.end();
+        });
+    }
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+myServer.listen(port,hostname, () => {
+    console.log(`server is running at http://${hostname}:${port}`);
 });
---
+++ student.js
@@ -0,0 +1,15 @@
+const getName = () => {
+    return "Kazi Riyad";
+}
+
+const getAge = () => {
+    return "25";
+};
+
+// exports.getName = getName;
+// exports.getAge = getAge;
+
+module.exports = {
+    getName,
+    getAge
+}
